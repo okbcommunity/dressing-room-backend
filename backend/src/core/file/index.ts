17101ba83d23ded7916608644f7f19021836f36b
benno <bennokohrs@gmail.com>
2022-11-17 17:45:11 +0100

    fixed typo

--- backend/src/core/file/index.ts
+++ backend/src/core/file/index.ts
@@ -1,28 +1,24 @@
 import fs from 'fs';
 
 export async function readFile(filepath: string): Promise<Buffer> {
-  console.info('Info: Start Reading File', filepath);
   return await new Promise((resolve, reject) => {
     fs.readFile(filepath, (err, content) => {
       if (err) {
         console.error('Error: Reading File', { filepath });
         reject(err);
       }
-      console.info('Info: End Reading File', { filepath });
       resolve(content);
     });
   });
 }
 
 export async function readDir(dirpath: string): Promise<string[]> {
-  console.info('Info: Start Reading Dir', dirpath);
   return await new Promise((resolve, reject) => {
     fs.readdir(dirpath, (err, filenames) => {
       if (err) {
         console.error('Error: Reading Dir', { dirpath });
         reject(err);
       }
-      console.info('Info: End Reading Dir', { dirpath, filenames });
       resolve(filenames);
     });
   });
@@ -34,18 +30,23 @@ export async function readFilesFromDir(
 ): Promise<Record<string, Buffer>> {
   const filesObject: Record<string, Buffer> = {};
   const filesInDir = await readDir(dirpath);
+
   for (const key in filesInDir) {
     const filename = filesInDir[key];
+    // Read shallow File
     if (isFile(`${dirpath}/${filename}`)) {
       const file = await readFile(`${dirpath}/${filename}`);
       filesObject[filename] = file;
-    } else if (deep && isDir(`${dirpath}/${filename}`)) {
+    }
+    // Read deep if Folder
+    else if (deep && isDir(`${dirpath}/${filename}`)) {
       const deepFiles = await readFilesFromDir(`${dirpath}/${filename}`, true);
       for (const deepFileKey of Object.keys(deepFiles)) {
         filesObject[`${filename}/${deepFileKey}`] = deepFiles[deepFileKey];
       }
     }
   }
+
   return filesObject;
 }
 
